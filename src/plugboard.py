ae2ec392fe87614126462d24e33787d2f65bea8d
ethanjpark <park.ethan@u.northwestern.edu>
2019-10-24 12:01:22 -0500

    rotor and reflector wirings and notches, plus the plugboard settings

---
+++ src/plugboard.py
@@ -0,0 +1,42 @@
+""" Plugboard pairings
+
+	Up to 10 alphabetic pairings could be made via physical wire connections.
+	This was the final component of the encryption before the accompanying lamp
+	would be lit for a keypress.
+
+	Taken from the 31st, 30th, and 29th from keylist #649, link below:
+	https://en.wikipedia.org/wiki/Enigma_machine#/media/File:Enigma_keylist_3_rotor.jpg
+"""
+
+plugboard31 = { 's':'z',
+				'g':'t',
+				'd':'v',
+				'k':'u',
+				'f':'o',
+				'm':'y',
+				'e':'w',
+				'j':'n',
+				'i':'x',
+				'l':'q' }
+
+plugboard30 = { 'i':'s',
+				'e':'v',
+				'm':'x',
+				'r':'w',
+				'd':'t',
+				'u':'z',
+				'j':'q',
+				'a':'o',
+				'c':'h',
+				'n':'y' }
+
+plugboard29 = { 'd':'j',
+				'a':'t',
+				'c':'v',
+				'i':'o',
+				'e':'r',
+				'q':'s',
+				'l':'w',
+				'p':'z',
+				'f':'n',
+				'b':'h' }
