b76df96538acbdf89e44c54f7e22333e86aa4a2a
Ethan Park <park.ethan@u.northwestern.edu>
2019-10-24 23:21:45 -0500

    Added link to wite with good explanation of ring setting and rotor starting pos, and modified input processing to include rotor starting positions.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://en.wikipedia.org/wiki/Enigma_rotor_details
 
 3-rotor configurations and plugboard configurations from here:
 https://en.wikipedia.org/wiki/Enigma_machine#/media/File:Enigma_keylist_3_rotor.jpg
+
+Ring setting (ringstellung) and rotor starting position explained here: (easy to confuse them)
+http://users.telenet.be/d.rijmenants/en/enigmatech.htm
--- src/input.py
+++ src/input.py
@@ -2,21 +2,20 @@
 The main enigma encryption process code.
 
 User will input 3 rotors (I,II,III,IV,V), the reflector (A,B,C),
-the rotor positions (3 numbers, 1-26), and plugboard configuration (29, 30, 31)
+ring settings (3 numbers between 1~26), rotor start positions (3 letters), 
+and plugboard configuration (29, 30, 31)
 
 Once this is done, then user will type in message which will then be encrypted or
 decrypted depending on which flag was raised initially.
 
 Or can use preset configuration (from key list #649), 29th, 30th, or 31st,
-by specifying either 29, 30, or 31 after the flag.
+by specifying either 29, 30, or 31 after the flag. User still needs to specify
+rotor starting positions.
 """
 
 from rotors.py import *
 from plugboard.py import *
 import sys
-
-alphabet = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
-			'q','r','s','t','u','v','w','x','y','z']
 
 #holds the appropriate rotor dictionary (from rotors.py) and the notch locations
 left_rotor = None
@@ -35,16 +34,16 @@ curr_mid_rotor = None
 curr_right_rotor = None
 
 #check for correct # of args depending on custom config (9) or preset (2)
-if(len(sys.argv)!=9 and len(sys.argv)!=2):
+if(len(sys.argv)!=12 and len(sys.argv)!=5):
 
 	print("Incorrect usage. Please see below for examples.\n")
-	print("Custom configuration: input.py I II III A 1 2 3 29\n")
-	print("Preset: input.py 29")
+	print("Custom configuration: input.py I II III A 1 2 3 A B C 29\n")
+	print("Preset: input.py 29 A B C")
 	sys.exit()
 
 else:
 	#assign correct dictionaries and values to the global variables based on inputs
-	if(len(sys.argv)==9): #custom configuration
+	if(len(sys.argv)==12): #custom configuration
 		
 		if(sys.argv[1]=='I'):
 			left_rotor = rotorI
@@ -109,11 +108,11 @@ else:
 		else:
 			sys.exit("Invalid reflector, exiting")
 
-		if(sys.argv[8]==29):
+		if(sys.argv[11]==29):
 			plugboard = plugboard29
-		elif(sys.argv[8]==30):
+		elif(sys.argv[11]==30):
 			plugboard = plugboard30
-		elif(sys.argv[8]==31):
+		elif(sys.argv[11]==31):
 			plugboard = plugboard31
 		else:
 			sys.exit("Invalid plugboard, exiting")
@@ -128,3 +127,19 @@ else:
 			mid_notch = notchII
 			right_rotor = rotorI
 			right_notch = notchI
+		elif(sys.argv[1]==30):
+			left_rotor = rotorIV
+			left_notch = notchIV
+			mid_rotor = rotorIII
+			mid_notch = notchIII
+			right_rotor = rotorII
+			right_notch = notchII
+		elif(sys.argv[1]==31):
+			left_rotor = rotorI
+			left_notch = notchI
+			mid_rotor = rotorV
+			mid_notch = notchV
+			right_rotor = rotorIII
+			right_notch = notchIII
+		else:
+			sys.exit("Invalid preset, exiting")
